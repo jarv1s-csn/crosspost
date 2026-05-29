e03330273e80f8410b3b16ff51a935b257fd7278
jarv1s-csn <909730873@qq.com>
2026-05-29 15:25:02 +0800

    fix: move popup entry to src/ and add extension icons

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import "./src/styles/global.css"
-import { AppLayout } from "./src/components/Layout/AppLayout"
+import "./styles/global.css"
+import { AppLayout } from "./components/Layout/AppLayout"
 
 function IndexPopup() {
   return <AppLayout />
