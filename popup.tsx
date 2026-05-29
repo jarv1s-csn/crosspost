c1abc8cd1eb998c0e2e2eb362134047b2a3fe343
Jarv1s <909730873@qq.com>
2026-05-29 13:19:35 +0800

    chore: scaffold Plasmo + React + TS

---
+++ popup.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function IndexPopup() {
+  return (
+    <div style={{ padding: 16, minWidth: 200 }}>
+      <h2>CrossPost</h2>
+    </div>
+  )
+}
+
+export default IndexPopup
